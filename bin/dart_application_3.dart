1b7ee630e7a237577b5afc164f3a03806158ddab
Hassan <hassan@gmail.com>
2026-01-20 20:19:39 +0300

    third

--- bin/dart_application_3.dart
+++ bin/dart_application_3.dart
@@ -1,7 +1,10 @@
 void main (){
   print("Hello world");
+  person p1 = person(age: 30, name: "moha");
+  print(p1.name);
 }
 class person {
 int? age;
 String? name;
+person({this.age,this.name});
 }
